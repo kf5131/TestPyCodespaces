7ecbaf624e104e020acf545ff33590ffd3582a5d
kf5131 <104850395+kf5131@users.noreply.github.com>
2023-12-27 16:56:26 +0000

    add drop_piece

--- C4-cli/main.py
+++ C4-cli/main.py
@@ -29,6 +29,26 @@ def print_board(board: list, status: dict = STATUS_INIT):
     
     ...
 
+def drop_piece(board: list, player:str = 'D', col: int = 1):
+    ''' Simple function for dropping pieces
+            default player is 'D' == de-bugger
+            default col is 1 for de-bugging
+        IN:
+        board: l.o.l. of str
+        player: str ('O' or 'X')
+        col: int (1 - 7)
+        OUT:
+        board: l.o.l. of str
+        '''
+    row = len(board) - 1  # Get the last row index
+    
+    while row >= 0:
+        if board[row][col-1] == ' ':  # Check if the cell is empty
+            board[row][col-1] = player  # Place the player's piece in the cell
+            break
+        row -= 1  # Move to the previous row
+    
+    return board
 
 def gameloop(board):
     
@@ -49,10 +69,13 @@ def main():
     
     board_1 = empty_board()
     
-    board_1[1][1] = PLAYER1
+    board_1[-1][-1:] = PLAYER1
     
     print_board(board_1)
     
+    board_2 = drop_piece(board_1, col=7)
+    
+    print_board(board_2)
     
     print('End work')
     ...
