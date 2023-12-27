6b05e4f1563ec75bc489c3069c0819238d9a000c
kf5131 <104850395+kf5131@users.noreply.github.com>
2023-12-27 16:47:37 +0000

    fix printboard

--- C4-cli/main.py
+++ C4-cli/main.py
@@ -8,6 +8,8 @@ BOARD_WIDTH: int = 7
 PLAYER0 = 'O'
 PLAYER1 = 'X'
 
+INBETWEENCHR = ' | '
+
 STATUS_INIT: dict = {'win': False, 'next': PLAYER1}
 
 
@@ -18,14 +20,12 @@ def empty_board(height: int = BOARD_HEIGHT, width:int = BOARD_WIDTH) -> list:
 
 def print_board(board: list, status: dict = STATUS_INIT):
     ''' Simple function to print board to CLI '''
-    print(status)
-    print(f"1 2 3 4 5 6 7")
+    print(f"Win is: {status['win']} & Next player is: {status['next']}")
+    print(f"1   2   3   4   5   6   7")
     
     for row in board:
-        out = ''
-        for chr in row:
-            out.join(chr)
-        print(out)    
+        out = INBETWEENCHR.join(row)  # Join the characters in the row with the INBETWEENCHR separator
+        print(out)  # Print the joined row
     
     ...
 
@@ -49,7 +49,7 @@ def main():
     
     board_1 = empty_board()
     
-    
+    board_1[1][1] = PLAYER1
     
     print_board(board_1)
     
